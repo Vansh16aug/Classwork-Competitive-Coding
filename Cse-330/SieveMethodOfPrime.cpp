eeadc5a41f4847a49dc7d2e4fbfbad6804da5f1c
Vansh16aug <kumarvansh16aug@gmail.com>
2024-08-30 16:51:04 +0530

    sieve-prime

---
+++ Cse-330/SieveMethodOfPrime.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Sieve(long long n)
+{
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for (long long i = 2; i * i <= n; i++)
+    {
+        if (isPrime[i])
+        {
+            for (long long j = i * i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+    for (long long i = 0; i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            cout << i << " ";
+        }
+    }
+}
+int main()
+{
+    long long n = 50;
+    Sieve(n);
+    return 0;
+}
